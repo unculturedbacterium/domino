a1cf91f36c1694a3f0eb83a59f5d9d3b0976ce6a
Mohammad Sadegi <mosadegi02@gmail.com>
2026-09-10 07:58:43 -0700

    Update __init__.py

--- domino/__init__.py
+++ domino/__init__.py
@@ -6,6 +6,8 @@ Public API
     PlinkReader      streaming PLINK1 reader
     compute_loco_grms, compute_grm
     estimate_h2, whiten_y, blup_resid
+    calculate_blups, calculate_component_blups, run_blup, write_blups
+    input QC, relationship summaries, GWAS reporting, and plotting helpers
     scan_chromosome
     degree_of_dominance, classify_da
 """
@@ -19,7 +21,35 @@ from .vc import (
 from .assoc import scan_chromosome, scan_chromosome_gls_eigen, iter_chromosome_gls_eigen
 from .classify import degree_of_dominance, classify_da, classify_inheritance, DEFAULT_THRESHOLDS
 from .pipeline import run_gwas
+from .blup import calculate_blups, calculate_component_blups, run_blup, write_blups
 from .resources import ResourceConfig, ExecutionPlan, plan_execution
+from .qc import (
+    align_phenotype_to_fam,
+    complete_case_traits,
+    encode_covariates,
+    plink_paths,
+    read_gcta_grm,
+    select_traits_by_prefix,
+    summarize_plink,
+    validate_plink_files,
+)
+from .relationships import (
+    eigensystem_summary,
+    estimate_heritability_table,
+    score_heritability_table,
+    summarize_grm,
+    summarize_loco_grms,
+)
+from .reporting import (
+    bonferroni_threshold,
+    compare_rankings,
+    dominance_class_counts,
+    genomic_inflation,
+    summarize_gwas_results,
+    top_hits,
+)
+from .plotting import additive_dominance_scatter, manhattan_plot, qq_plot, runtime_memory_plot
+from .reproducibility import collect_environment, public_api_table, write_manifest
 
 __version__ = "1.0.0"
 __all__ = [
@@ -28,8 +58,18 @@ __all__ = [
     "estimate_h2", "estimate_h2_many", "score_variance_components",
     "multivariate_score_transform", "whiten_y", "whiten_matrix",
     "covariance_matrix", "blup_resid", "scan_chromosome",
+    "calculate_blups", "calculate_component_blups", "run_blup", "write_blups",
     "scan_chromosome_gls_eigen", "iter_chromosome_gls_eigen",
     "ResourceConfig", "ExecutionPlan", "plan_execution",
+    "align_phenotype_to_fam", "complete_case_traits", "encode_covariates",
+    "plink_paths", "read_gcta_grm", "select_traits_by_prefix",
+    "summarize_plink", "validate_plink_files", "eigensystem_summary",
+    "estimate_heritability_table", "score_heritability_table",
+    "summarize_grm", "summarize_loco_grms", "bonferroni_threshold",
+    "compare_rankings", "dominance_class_counts", "genomic_inflation",
+    "summarize_gwas_results", "top_hits", "collect_environment",
+    "public_api_table", "write_manifest", "additive_dominance_scatter",
+    "manhattan_plot", "qq_plot", "runtime_memory_plot",
     "degree_of_dominance", "classify_da", "classify_inheritance",
     "DEFAULT_THRESHOLDS", "__version__",
 ]
